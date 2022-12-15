9638e74a0e6c841f459b95a0e06ac5b03a339e90
gonzagiorgis <108843917+gonzagiorgis@users.noreply.github.com>
2022-12-15 00:11:11 -0300

    correccion counter

--- src/componentes/ItemCount.jsx
+++ src/componentes/ItemCount.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 
 const ItemCount = ({ stock }) => {
-  const [cantidad, setCantidad] = useState(0);
-  const [stockItem, setStock] = useState(stock);
+  const [cantidad, setCantidad] = useState(1);
+  const [stockItem, setStockItem] = useState(0);
+
+  // no sumaba al usar useState(stock), solucione el problema inicializando stockItem con la prop stock de manera asincrónica ya que proviene de dicha forma desde ItemDetailContainer, de otra manera indicaba stockItem como undefined y no sumaba con incrementar()
+  useEffect(() => {
+    const promesa = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(stock);
+      });
+    });
+    promesa.then((dato) => {
+      setStockItem(dato);
+    });
+  }, [stock]);
 
   const incrementar = () => {
     if (cantidad < stockItem) {
@@ -19,11 +32,11 @@ const ItemCount = ({ stock }) => {
 
   const onAdd = () => {
     if (cantidad <= stockItem) {
-      setStock(stockItem - cantidad);
+      setStockItem(stockItem - cantidad);
       setCantidad(0);
     }
     console.log(`Agragaste ${cantidad} al carrito`);
-    console.log(stock);
+    console.log(stockItem);
   };
 
   return (
