81584269a5e4ef303b8cf500970e94b4f65f61ab
gonzagiorgis <108843917+gonzagiorgis@users.noreply.github.com>
2022-12-20 17:51:20 -0300

    responsive menor sm

--- src/componentes/ItemList.jsx
+++ src/componentes/ItemList.jsx
@@ -5,7 +5,10 @@ const ItemList = ({ items }) => {
   return (
     <div className="row">
       {items.map((item) => (
-        <div className="col-sm-6 col-lg-4 col-xl-3 mb-2" key={item.id}>
+        <div
+          className="d-flex justify-content-center col-sm-6 col-lg-4 col-xl-3 mb-2"
+          key={item.id}
+        >
           <Item item={item} />
         </div>
       ))}
